a394859d2f9a3a344a21737aa34d354e0fe1f36e
Anas Badr <anasihabezmc@gmail.com>
2023-10-26 23:50:28 +0300

    Initial commit (Adding project files)

---
+++ unnamed_music_visualizer/main.py
@@ -0,0 +1,23 @@
+import pygame
+
+
+class Game:
+    def __init__(self):
+        """Initializes the game object."""
+        pass
+
+    def run(self):
+        """Run the game."""
+        pass
+
+    def update(self):
+        """Update the game."""
+        pass
+
+    def quit(self):
+        """Quit the game."""
+        pass
+
+    def load_sound(self):
+        """Lload the sound that's going to be visualized."""
+        pass
